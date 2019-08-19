0dc9e3e8efb3272a417f4910c0cab17b72fef60c
Rafael Stahl <r.stahl@tum.de>
2019-08-19 19:53:07 +0200

    riscv example: working virtual dispatch, rtti, static local init; some work on exceptions, but needs newlib recompile

--- examples/SW/riscv_cpp/main.cpp
+++ examples/SW/riscv_cpp/main.cpp
@@ -1,6 +1,18 @@
 #include <vector>
 #include <cstdio>
 #include <iostream>
+#include <memory>
+
+class Foo
+{
+public:
+    virtual void moo() const { printf("foo!\n"); }
+};
+class Cow : public Foo
+{
+public:
+    void moo() const override { printf("moo!\n"); }
+};
 
 int main()
 {
@@ -10,6 +22,24 @@ int main()
     printf("===== hello world %i %c %08X =====\n", 123, 123, 123);
 
     std::cout << "hello c++ world" << std::endl;
+
+    auto f = std::make_unique<Cow>();
+    f->moo();
+    Foo *foo = f.get();
+    Cow *cow = dynamic_cast<Cow*>(foo);
+    cow->moo();
+
+    // Fails because of hardfloats too at: _Unwind_RaiseException+82
+    /*try
+    {
+        throw 5;
+    }
+    catch (int i)
+    {
+        printf("caught: %i\n", i);
+    }*/
+
+    static int threadsafe_init = 7;
 
     std::vector<int> v;
     v.push_back(1);
