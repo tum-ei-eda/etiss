edc4246eff183c42dc2d811872862fd1fd462583
Andrew Stevens <andrew.stevens@infineon.com>
2019-12-03 23:21:49 +0100

    Fixed: support for specifying INICIO-compliant BOOST_ROOT
         Note: to use cmake support must used full versioned --layout for boost.build

--- IFX_CONFIG.sh
+++ IFX_CONFIG.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 source ../SETTINGS_AND_VERSIONS.sh
 
 ETISS_HOME="$PREFIX/etiss-${ETISS_VERSION}" 
@@ -6,9 +7,21 @@ export CC=`which gcc`
 export CXX=`which g++`
 BUILDDIR=Release
 mkdir -p ${BUILDDIR} 
-( cd ${BUILDDIR} ; 
- cmake -DCMAKE_BUILD_TYPE=${BUILDDIR} -DCMAKE_VERBOSE_MAKEFILE=TRUE -DETISS_BUILD_MANUAL_DOC=ON -DETISS_USE_PYTHON=OFF \
-  -DCMAKE_INSTALL_PREFIX:PATH="$ETISS_HOME" --graphviz=test.dot -G "CodeBlocks - Unix Makefiles" ..
+if [[ -n "$BOOST_ROOT" ]]
+then
+    SET_BOOST_ROOT=-DBoost_ROOT="${BOOST_ROOT}"
+fi
+
+CMAKE_OPTS=(
+   -DCMAKE_BUILD_TYPE=${BUILDDIR} \
+	 ${SET_BOOST_ROOT} -DBoost_COMPILER=mgw92 \
+	 -DCMAKE_VERBOSE_MAKEFILE=TRUE -DETISS_BUILD_MANUAL_DOC=ON -DETISS_USE_PYTHON=OFF \
+	 -DCMAKE_COLOR_MAKEFILE=OFF \
+         -DCMAKE_INSTALL_PREFIX:PATH="$ETISS_HOME"
+)
+( cd ${BUILDDIR} ;
+  echo CMAKE_OPTS "${CMAKE_OPTS[@]}" ;\
+ cmake "${CMAKE_OPTS[@]}" --graphviz=test.dot -G "$CMAKE_GEN" ..
 )
 echo 
 echo "make -C ${BUILDDIR} target... - to compile selected configuration"
