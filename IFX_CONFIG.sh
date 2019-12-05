bf59c3d46fe7ed4d2ef425dc384c5b231042edd5
Andrew Stevens <andrew.stevens@infineon.com>
2019-12-05 18:35:23 +0100

    Fixed: make IFX_CONFIG.sh useable for examples
        TODO: make it re-useable IFX_CONFIG.sh aiml_deployment useable in all projects.

--- IFX_CONFIG.sh
+++ IFX_CONFIG.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 set -e
-source ../SETTINGS_AND_VERSIONS.sh
+source `dirname "$0"`/../SETTINGS_AND_VERSIONS.sh
+
 
 ETISS_HOME="$PREFIX/etiss-${ETISS_VERSION}" 
+INSTALL_HOME="${ETISS_HOME}"
+
+while [[ "$1" != "" && "$1" != "--" ]]
+do
+    case "$1" in
+    "--help"|"-?") 
+        echo "`basename $0`: --install <install_path> "
+        exit 1
+        ;;
+    "--install"|"-I")
+        INSTALL_HOME="$2"
+        shift
+        ;;
+    "*")
+        break
+        ;;
+    esac
+    shift
+done
+
 export CC=`which gcc`
 export CXX=`which g++`
 BUILDDIR=Build-${HOST_TC_ID}
@@ -17,12 +38,12 @@ CMAKE_OPTS=(
 	 ${SET_BOOST_ROOT} -DBoost_COMPILER=mgw92 \
 	 -DCMAKE_VERBOSE_MAKEFILE=TRUE -DETISS_BUILD_MANUAL_DOC=ON -DETISS_USE_PYTHON=OFF \
 	 -DCMAKE_COLOR_MAKEFILE=OFF \
-         -DCMAKE_INSTALL_PREFIX:PATH="$ETISS_HOME"
+         -DCMAKE_INSTALL_PREFIX:PATH="$INSTALL_HOME"
 )
-( cd ${BUILDDIR} ;
-  echo CMAKE_OPTS "${CMAKE_OPTS[@]}" ;\
- cmake "${CMAKE_OPTS[@]}" --graphviz=test.dot -G "$CMAKE_GEN" ..
-)
+
+echo CMAKE_OPTS "${CMAKE_OPTS[@]}" ;
+cmake -B  "${BUILDDIR}" -S . "${CMAKE_OPTS[@]}" --graphviz=test.dot -G "$CMAKE_GEN" 
+
 echo 
-echo "make -C ${BUILDDIR} target... - to compile selected configuration"
+echo "cmake --build ${BUILDDIR} ... - to compile selected configuration"
 echo
