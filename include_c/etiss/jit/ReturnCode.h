09efa887a4daa733cb76f26d2b087715d8d6d4b3
Rafael Stahl <r.stahl@tum.de>
2019-12-10 10:55:25 +0100

    improve formatting

--- include_c/etiss/jit/ReturnCode.h
+++ include_c/etiss/jit/ReturnCode.h
@@ -156,60 +156,49 @@ extern int32_t etiss_returncode_setTemporary(const char *msg);
 
 //////////////////// definition of global (cpu architecture independent) return codes
 
-defineReturnCode(NOERROR, 0, "No error.") defineReturnCode(GENERALERROR, -1, "General error.")
-
-    defineReturnCode(RELOADBLOCKS, -2, "Clear cached translated blocks.")
-        defineReturnCode(RELOADCURRENTBLOCK, -3, "Clear the cached block that returned this code.")
-    // defineReturnCode(RELOADRANGE,-4,"clear cached translated blocks in a range")
-
-    defineReturnCode(DBUS_READ_ERROR, -5, "Data bus read error.") defineReturnCode(DBUS_WRITE_ERROR, -6,
-                                                                                   "Data bus write error.")
-        defineReturnCode(IBUS_READ_ERROR, -7,
-                         "Instruction bus read error.") defineReturnCode(IBUS_WRITE_ERROR, -8,
-                                                                         "Instruction bus write error.")
-
-            defineReturnCode(INTERRUPT, -9, "Signal an interrupt.") defineReturnCode(RESET, -10, "Reset signal.")
-
-                defineReturnCode(ILLEGALINSTRUCTION, -11,
-                                 "Illegal instruction.") defineReturnCode(ILLEGALJUMP, -12, "Illegal jump address.")
-                    defineReturnCode(INSTR_PAGEFAULT, -13, "Instruction page fault.") defineReturnCode(
-                        LOAD_PAGEFAULT, -14, "Load page fault.") defineReturnCode(STORE_PAGEFAULT, -15,
-                                                                                  "Store page fault.")
-
-                        defineReturnCode(GDBNOERROR, -16,
-                                         "Has the same effect as NOERROR but allows exiting a block where NOERROR "
-                                         "would have continued block execution.")
-
-                            defineReturnCode(SYSCALL, -17, "System call") defineReturnCode(
-                                PAGEFAULT, -18, "Virtual memory tranlation fault.") defineReturnCode(BREAKPOINT, -19,
-                                                                                                     "Break point.")
-
-                                defineReturnCode(CPUFINISHED, 1 << 31,
-                                                 "Finished cpu execution. This is the proper way to exit from "
-                                                 "etiss::CPUCore::execute.")
-                                    defineReturnCode(CPUTERMINATED, 0x80000001,
-                                                     "Terminates cpu execution. this is the proper way to force an "
-                                                     "exit from etiss::CPUCore::execute (e.g. by a debugger).")
-
-                                        defineReturnCode(JITERROR, -32, "Error in JIT.")
-                                            defineReturnCode(JITCOMPILATIONERROR, -33, "Error in JIT compilation.")
-                                                defineReturnCode(ARCHERROR, -34, "Error in architecture.")
-                                                    defineReturnCode(EMULATIONNOTSUPPORTED, -35,
-                                                                     "Emulation is not supported.")
-
-                                                        defineReturnCode(INVALIDSYSTEM, -36,
-                                                                         "An ETISS_System structure doesn't contain "
-                                                                         "the neccessary function pointers.")
-
-                                                            defineReturnCode(TEMPORARY0, -128,
-                                                                             "") defineReturnCode(TEMPORARY1, -129, "")
-                                                                defineReturnCode(TEMPORARY2, -130, "")
-                                                                    defineReturnCode(TEMPORARY3, -131, "")
-                                                                        defineReturnCode(TEMPORARY4, -132, "")
-                                                                            defineReturnCode(TEMPORARY5, -133, "")
-                                                                                defineReturnCode(TEMPORARY6, -134, "")
-                                                                                    defineReturnCode(TEMPORARY7, -135,
-                                                                                                     "")
+defineReturnCode(NOERROR, 0, "No error.");
+defineReturnCode(GENERALERROR, -1, "General error.");
+defineReturnCode(RELOADBLOCKS, -2, "Clear cached translated blocks.");
+defineReturnCode(RELOADCURRENTBLOCK, -3, "Clear the cached block that returned this code.");
+// defineReturnCode(RELOADRANGE,-4,"clear cached translated blocks in a range")
+defineReturnCode(DBUS_READ_ERROR, -5, "Data bus read error.");
+defineReturnCode(DBUS_WRITE_ERROR, -6, "Data bus write error.");
+defineReturnCode(IBUS_READ_ERROR, -7, "Instruction bus read error.");
+defineReturnCode(IBUS_WRITE_ERROR, -8, "Instruction bus write error.");
+defineReturnCode(INTERRUPT, -9, "Signal an interrupt.");
+defineReturnCode(RESET, -10, "Reset signal.");
+defineReturnCode(ILLEGALINSTRUCTION, -11, "Illegal instruction.");
+defineReturnCode(ILLEGALJUMP, -12, "Illegal jump address.");
+defineReturnCode(INSTR_PAGEFAULT, -13, "Instruction page fault.");
+defineReturnCode(LOAD_PAGEFAULT, -14, "Load page fault.");
+defineReturnCode(STORE_PAGEFAULT, -15, "Store page fault.");
+defineReturnCode(GDBNOERROR, -16,
+                 "Has the same effect as NOERROR but allows exiting a block where NOERROR "
+                 "would have continued block execution.");
+defineReturnCode(SYSCALL, -17, "System call");
+defineReturnCode(PAGEFAULT, -18, "Virtual memory tranlation fault.");
+defineReturnCode(BREAKPOINT, -19, "Break point.");
+defineReturnCode(CPUFINISHED, 1 << 31,
+                 "Finished cpu execution. This is the proper way to exit from "
+                 "etiss::CPUCore::execute.");
+defineReturnCode(CPUTERMINATED, 0x80000001,
+                 "Terminates cpu execution. this is the proper way to force an "
+                 "exit from etiss::CPUCore::execute (e.g. by a debugger).");
+defineReturnCode(JITERROR, -32, "Error in JIT.");
+defineReturnCode(JITCOMPILATIONERROR, -33, "Error in JIT compilation.");
+defineReturnCode(ARCHERROR, -34, "Error in architecture.");
+defineReturnCode(EMULATIONNOTSUPPORTED, -35, "Emulation is not supported.");
+defineReturnCode(INVALIDSYSTEM, -36,
+                 "An ETISS_System structure doesn't contain "
+                 "the neccessary function pointers.");
+defineReturnCode(TEMPORARY0, -128, "");
+defineReturnCode(TEMPORARY1, -129, "");
+defineReturnCode(TEMPORARY2, -130, "");
+defineReturnCode(TEMPORARY3, -131, "");
+defineReturnCode(TEMPORARY4, -132, "");
+defineReturnCode(TEMPORARY5, -133, "");
+defineReturnCode(TEMPORARY6, -134, "");
+defineReturnCode(TEMPORARY7, -135, "");
 #define ETISS_RETURNCODE_TEMPORARY_COUNT 8
 
 #endif
