6bcd4137975710b8b15f9c1beb1cc5f383620141
sharif <uzair.sharif@tum.de>
2020-06-02 16:45:49 +0200

    patch to allow linking with ETISS in its build tree

--- cmake/RegisterJITFiles.cmake
+++ cmake/RegisterJITFiles.cmake
@@ -6,9 +6,19 @@ MACRO(RegisterJITFiles Files)
         LIST(GET FilePair -1 ExistingFile)
         GET_FILENAME_COMPONENT(TargetPath ${TargetFile} DIRECTORY)
         INSTALL(FILES "${ExistingFile}" DESTINATION "include/jit/${TargetPath}")
+
+
+        # mimicing installation in build tree as well
+        # NOTE: pluginexport hasn't been generated yet so will be copied later
+        if (NOT "${ExistingFile}" STREQUAL "${ETISS_BINARY_DIR}/include/etiss/pluginexport.h")
+          file(COPY "${ExistingFile}" DESTINATION "${ETISS_BINARY_DIR}/include/jit/${TargetPath}")
+        endif()
     ENDFOREACH()
 ENDMACRO()
 
 MACRO(RegisterJITFileArch Arch)
     INSTALL(FILES "${CMAKE_CURRENT_LIST_DIR}/${Arch}.h" DESTINATION "include/jit/Arch/${Arch}")
+
+    # mimicing installation in build tree as well
+    file(COPY "${CMAKE_CURRENT_LIST_DIR}/${Arch}.h" DESTINATION "${ETISS_BINARY_DIR}/include/jit/Arch/${Arch}")
 ENDMACRO()
