7259138cfe85711a92e7b5cb62b7d4f88b1eacf9
Rafael Stahl <r.stahl@tum.de>
2020-06-16 17:15:50 +0200

    formatting

--- include_c/etiss/jit/resources.cpp
+++ include_c/etiss/jit/resources.cpp
@@ -1,38 +1,49 @@
 #include <cstdint>
 #include "CPU.h"
-extern "C" {
-	uint32_t handleResources(uint32_t resource_time[100], uint32_t resources[100][100], uint32_t num_stages, uint32_t num_resources[100], ETISS_CPU *cpu){
-		for(int i = 0; i < num_stages; i++){
-			int max = 0;
-			for(int j = 0; j < num_resources[i]; j++){//set "in use" metric
-				cpu->resourceUsages[resources[i][j]] += resource_time[resources[i][j]];
-			}
-			for(int j = 0; j < num_resources[i]; j++){//find max in this stage	
-				if(cpu->cycles[resources[i][j]] > max){
-					max = cpu->cycles[resources[i][j]];
-				}
-			}
+extern "C"
+{
+    uint32_t handleResources(uint32_t resource_time[100], uint32_t resources[100][100], uint32_t num_stages,
+                             uint32_t num_resources[100], ETISS_CPU *cpu)
+    {
+        for (int i = 0; i < num_stages; i++)
+        {
+            int max = 0;
+            for (int j = 0; j < num_resources[i]; j++)
+            { // set "in use" metric
+                cpu->resourceUsages[resources[i][j]] += resource_time[resources[i][j]];
+            }
+            for (int j = 0; j < num_resources[i]; j++)
+            { // find max in this stage
+                if (cpu->cycles[resources[i][j]] > max)
+                {
+                    max = cpu->cycles[resources[i][j]];
+                }
+            }
 
-			if (!(i == 0)){//find max in stage before this stage
-				for(int j = 0; j<num_resources[i-1]; j++){
-					if(cpu->cycles[resources[i-1][j]] > max){
-						max = cpu->cycles[resources[i-1][j]];
-					}
-				}
-			}
-			int max_stagetime = 0;
-			for(int j = 0; j < num_resources[i]; j++){//find max time in stage
-				if (resource_time[resources[i][j] > max_stagetime]){
-					max_stagetime = resource_time[resources[i][j]];
-				}
-			}
+            if (!(i == 0))
+            { // find max in stage before this stage
+                for (int j = 0; j < num_resources[i - 1]; j++)
+                {
+                    if (cpu->cycles[resources[i - 1][j]] > max)
+                    {
+                        max = cpu->cycles[resources[i - 1][j]];
+                    }
+                }
+            }
+            int max_stagetime = 0;
+            for (int j = 0; j < num_resources[i]; j++)
+            { // find max time in stage
+                if (resource_time[resources[i][j] > max_stagetime])
+                {
+                    max_stagetime = resource_time[resources[i][j]];
+                }
+            }
 
-			for(int j = 0; j<num_resources[i]; j++){//set new values
-				cpu->cycles[resources[i][j]] = max + max_stagetime;
-			}
-	
-		}
-		return 0;
-	}
-
+            for (int j = 0; j < num_resources[i]; j++)
+            { // set new values
+                cpu->cycles[resources[i][j]] = max + max_stagetime;
+            }
+        }
+        return 0;
+    }
 }
