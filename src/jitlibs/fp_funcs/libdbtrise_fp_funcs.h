347100b85879105b5404334f9c9f16b69fd1d840
Rafael Stahl <r.stahl@tum.de>
2020-06-17 11:00:33 +0200

    cmake: install missing jit header

--- src/jitlibs/fp_funcs/libdbtrise_fp_funcs.h
+++ src/jitlibs/fp_funcs/libdbtrise_fp_funcs.h
@@ -1,3 +1,8 @@
+#ifndef JITLIB_FPFUNCS_H
+#define JITLIB_FPFUNCS_H
+
+#include <stdint.h>
+
 /*header file for the static library dbtrise_fp_funcs*/
 uint32_t fget_flags();
 uint32_t fadd_s(uint32_t v1, uint32_t v2, uint8_t mode);
@@ -25,3 +30,5 @@ uint64_t fclass_d(uint64_t v1);
 uint64_t fcvt_32_64(uint32_t v1, uint32_t op, uint8_t mode);
 uint32_t fcvt_64_32(uint64_t v1, uint32_t op, uint8_t mode);
 uint32_t unbox_s(uint64_t v);
+
+#endif
--- src/jitlibs/resources/libresources.h
+++ src/jitlibs/resources/libresources.h
@@ -1,2 +1,12 @@
+#ifndef JITLIB_RESOURCES_H
+#define JITLIB_RESOURCES_H
+
+#include <stdint.h>
+
+struct ETISS_CPU;
+
 /*header file for the static library "resources"*/
-uint32_t handleResources(uint32_t resource_time[100], uint32_t resources[100][100], uint32_t num_stages, uint32_t num_resources[100], ETISS_CPU *cpu);
+uint32_t handleResources(uint32_t *resource_time, uint32_t **resources, uint32_t num_stages, uint32_t *num_resources,
+                         ETISS_CPU *cpu);
+
+#endif
