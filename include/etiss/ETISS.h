e5c51f414b6a978ba4ca4d8673409d69fb1a95b0
Johannes Geier <johannes.geier@tum.de>
2021-01-22 19:42:53 +0100

    remove dead etiss::initialize declaration to force uniform initialize usage

--- include/etiss/ETISS.h
+++ include/etiss/ETISS.h
@@ -207,8 +207,7 @@ std::set<std::string> listLibraryPrefixes();
  * @attention It is recommended to use etiss::Initializer instead of calling
  * etiss::initialize / etiss::shutdown manually
  */
-void initialize(std::vector<std::string> args);
-void initialize(int argc, const char* argv[]);
+void initialize(std::vector<std::string>& args);
 /**
  * @brief Get the default JIT implementation.
  *
@@ -338,20 +337,25 @@ class Initializer
         static_assert(std::is_same<const char, argvT>::value || std::is_same<char, argvT>::value,
                       "argv must be of type const char or char");
         std::vector<std::string> args;
-        for (int i = 0; i < argc; i++)
+        for (int i = 1; i < argc; i++)
         {
             args.push_back(std::string(argv[i]));
         }
         toList(args, args_append...);
         initialize(args);
     }
-    
+
     Initializer(std::list<std::string> *files, int argc, const char* argv[])
     {
+        std::vector<std::string> args;
+        for (int i = 1; i < argc; i++)
+        {
+            args.push_back(std::string(argv[i]));
+        }
         loadIni(files);
-        initialize(argc, argv);
+        initialize(args);
     }
-    
+
     /**
      * @brief Constructor that initializes ETISS.
      *
@@ -401,7 +405,7 @@ class Initializer
         static_assert(std::is_same<const char, argvT>::value || std::is_same<char, argvT>::value,
                       "argv must be of type const char or char");
         std::vector<std::string> args;
-        for (int i = 0; i < argc; i++)
+        for (int i = 1; i < argc; i++)
         {
             args.push_back(std::string(argv[i]));
         }
