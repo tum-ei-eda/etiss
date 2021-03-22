7de40d9f9f87a29b41710a8bf021e4c479d25976
AnoushkaU <anoushka.u3012@gmail.com>
2021-03-22 09:45:34 +0100

    toolchain path config

--- examples/SW/riscv/cmake/pulpino_tumeda/toolchain.cmake
+++ examples/SW/riscv/cmake/pulpino_tumeda/toolchain.cmake
@@ -8,9 +8,10 @@ IF(WIN32)
     set(RISCV_ARCH "rv32gc" CACHE STRING "march argument to the compiler")
     set(RISCV_ABI "ilp32d" CACHE STRING "mabi argument to the compiler")
 
-    set(CMAKE_C_COMPILER D:/a/etiss/riscv-tc/bin/${RISCV_ELF_GCC_BASENAME}-gcc${EXE_EXT})
-    set(CMAKE_CXX_COMPILER D:/a/etiss/riscv-tc/bin/${RISCV_ELF_GCC_BASENAME}-g++${EXE_EXT})
-    set(CMAKE_OBJCOPY D:/a/etiss/riscv-tc/bin/${RISCV_ELF_GCC_BASENAME}-objcopy${EXE_EXT})
+    set(TC_PREFIX "${RISCV_ELF_GCC_PREFIX}/bin/${RISCV_ELF_GCC_BASENAME}-")
+    set(CMAKE_C_COMPILER ${TC_PREFIX}gcc${EXE_EXT})
+    set(CMAKE_CXX_COMPILER ${TC_PREFIX}g++${EXE_EXT})
+    set(CMAKE_OBJCOPY ${TC_PREFIX}objcopy${EXE_EXT})
 
 ELSE()
     set(RISCV_ELF_GCC_PREFIX "/usr/local/research/projects/SystemDesign/tools/riscv/current" CACHE PATH "install location for riscv-gcc toolchain")
