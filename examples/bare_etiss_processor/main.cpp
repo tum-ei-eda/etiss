28db17941b166ebf6fa7e2bd96ce1d959c4c4f66
uzleosharif <64044808+uzleosharif@users.noreply.github.com>
2021-04-29 18:46:22 +0000

    supporting logpc option with run_helper.sh script

--- examples/bare_etiss_processor/main.cpp
+++ examples/bare_etiss_processor/main.cpp
@@ -123,8 +123,9 @@ int main(int argc, const char *argv[])
     initializer.loadIniJIT(cpu);
     // here own developped plug-ins can be added with:
     if (etiss::cfg().get<bool>("etiss.log_pc", false)) {
-      cpu->addPlugin(std::shared_ptr<etiss::Plugin>(new TracePrinter(0x8888, true)));
-    }
+      etiss::cfg().set<int>("etiss.max_block_size", 1);
+      cpu->addPlugin(std::shared_ptr<etiss::Plugin>(new TracePrinter(0x88888)));
+    }	
     
     std::cout << "=== Setting up plug-ins ===" << std::endl << std::endl;
 
