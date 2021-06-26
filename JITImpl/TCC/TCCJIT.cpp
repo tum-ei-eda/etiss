6a60c2c41edb99c3f302a2742bff5515dd2b1629
Rafael Stahl <r.stahl@tum.de>
2021-06-26 21:42:18 +0200

    ci: try fix windows

--- JITImpl/TCC/TCCJIT.cpp
+++ JITImpl/TCC/TCCJIT.cpp
@@ -102,8 +102,6 @@ TCCJIT::TCCJIT() : JIT("tcc")
 {
 #if ETISS_USE_GETPROC
     addAllSymbols(extsymbols);
-    for (const auto &jitPath : etiss::jitExtLibPaths())
-        etiss::LibraryInterface::AddSearchPath(jitPath);
 #endif
 }
 
@@ -143,6 +141,8 @@ void *TCCJIT::translate(std::string code, std::set<std::string> headerpaths, std
     {
         tcc_add_symbol(s, sym.first.c_str(), sym.second);
     }
+    for (const auto &jitPath : etiss::jitExtLibPaths())
+        etiss::LibraryInterface::AddSearchPath(etiss::jitFiles() + jitPath);
 #endif
 
     // init libs
