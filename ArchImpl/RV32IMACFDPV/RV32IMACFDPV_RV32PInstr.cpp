aea20776e0d6f88990856135b74ef0fe38ccadd8
Philipp van Kempen <philipp.van-kempen@tum.de>
2022-05-16 14:04:35 +0200

    RVP: Support ADD16 instruction

--- ArchImpl/RV32IMACFDPV/RV32IMACFDPV_RV32PInstr.cpp
+++ ArchImpl/RV32IMACFDPV/RV32IMACFDPV_RV32PInstr.cpp
@@ -89,6 +89,79 @@ ss << "add8" << " # " << ba << (" [rd=" + std::to_string(rd) + " | rs1=" + std::
 	}
 );
 
+// ADD16 -----------------------------------------------------------------------
+static InstructionDefinition add16_rd_rs1_rs2 (
+	ISA32_RV32IMACFDPV,
+	"add16",
+	(uint32_t) 0x40000077,
+	(uint32_t) 0xfe00707f,
+	[] (BitArray & ba,etiss::CodeSet & cs,InstructionContext & ic)
+	{
+
+// -----------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------
+etiss_uint32 rd = 0;
+static BitArrayRange R_rd_0(11, 7);
+rd += R_rd_0.read(ba) << 0;
+etiss_uint32 rs1 = 0;
+static BitArrayRange R_rs1_0(19, 15);
+rs1 += R_rs1_0.read(ba) << 0;
+etiss_uint32 rs2 = 0;
+static BitArrayRange R_rs2_0(24, 20);
+rs2 += R_rs2_0.read(ba) << 0;
+
+// -----------------------------------------------------------------------------
+
+		CodePart & partInit = cs.append(CodePart::INITIALREQUIRED);
+
+		partInit.code() = std::string("//ADD16\n");
+
+// -----------------------------------------------------------------------------
+partInit.code() += "cpu->instructionPointer = " + std::to_string(ic.current_address_ + 4U) + ";\n";
+if (rd != 0U) {
+partInit.code() += "etiss_uint32 rs1_val = *((RV32IMACFDPV*)cpu)->X[" + std::to_string(rs1) + "];\n";
+partInit.code() += "etiss_uint32 rs2_val = *((RV32IMACFDPV*)cpu)->X[" + std::to_string(rs2) + "];\n";
+partInit.code() += "etiss_uint16 rd_val0 = (((rs1_val) >> (0U)) & 65535) + (((rs2_val) >> (0U)) & 65535);\n";
+partInit.code() += "etiss_uint16 rd_val1 = (((rs1_val) >> (16U)) & 65535) + (((rs2_val) >> (16U)) & 65535);\n";
+partInit.code() += "etiss_uint32 rd_val = (((rd_val1) << 16) | (rd_val0));\n";
+partInit.code() += "*((RV32IMACFDPV*)cpu)->X[" + std::to_string(rd) + "] = rd_val;\n";
+}
+// -----------------------------------------------------------------------------
+
+		partInit.getRegisterDependencies().add(reg_name[rs1], 32);
+		partInit.getRegisterDependencies().add(reg_name[rs2], 32);
+		partInit.getAffectedRegisters().add(reg_name[rd], 32);
+		partInit.getAffectedRegisters().add("instructionPointer", 32);
+
+		return true;
+	},
+	0,
+	[] (BitArray & ba, Instruction & instr)
+	{
+// -----------------------------------------------------------------------------
+etiss_uint32 rd = 0;
+static BitArrayRange R_rd_0(11, 7);
+rd += R_rd_0.read(ba) << 0;
+etiss_uint32 rs1 = 0;
+static BitArrayRange R_rs1_0(19, 15);
+rs1 += R_rs1_0.read(ba) << 0;
+etiss_uint32 rs2 = 0;
+static BitArrayRange R_rs2_0(24, 20);
+rs2 += R_rs2_0.read(ba) << 0;
+
+// -----------------------------------------------------------------------------
+
+		std::stringstream ss;
+// -----------------------------------------------------------------------------
+ss << "add16" << " # " << ba << (" [rd=" + std::to_string(rd) + " | rs1=" + std::to_string(rs1) + " | rs2=" + std::to_string(rs2) + "]");
+// -----------------------------------------------------------------------------
+		return ss.str();
+	}
+);
+
 // SUB8 ------------------------------------------------------------------------
 static InstructionDefinition sub8_rd_rs1_rs2 (
 	ISA32_RV32IMACFDPV,
