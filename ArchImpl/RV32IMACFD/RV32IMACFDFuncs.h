4207834569a08255cb46786ea2199d6ab712c216
wysiwyng <4764286+wysiwyng@users.noreply.github.com>
2022-06-22 10:07:48 +0200

    regenerate funcs.h

--- ArchImpl/RV32IMACFD/RV32IMACFDFuncs.h
+++ ArchImpl/RV32IMACFD/RV32IMACFDFuncs.h
@@ -15,41 +15,9 @@
 #endif
 
 
-static inline etiss_int32 raise(ETISS_CPU * const cpu, ETISS_System * const system, void * const * const plugin_pointers, etiss_int32 irq, etiss_int32 mcause)
-{
-if (irq != 0U) {
-return -9U;
-} else {
-if (mcause == 0U || mcause == 1U) {
-return -7U;
-}
-if (mcause == 2U) {
-return -11U;
-}
-if (mcause == 3U) {
-return -19U;
-}
-if (mcause == 4U || mcause == 5U) {
-return -5U;
-}
-if (mcause == 6U || mcause == 7U) {
-return -6U;
-}
-if (mcause == 8U || mcause == 9U || mcause == 10U || mcause == 11U) {
-return -17U;
-}
-if (mcause == 12U || mcause == 20U) {
-return -13U;
-}
-if (mcause == 13U || mcause == 21U) {
-return -14U;
-}
-if (mcause == 15U || mcause == 23U) {
-return -15U;
-}
-return -11U;
-}
-}
+#ifndef ETISS_ARCH_STATIC_FN_ONLY
+static inline void raise(etiss_int32 irq, etiss_int32 mcause);
+#endif
 
 #ifndef ETISS_ARCH_STATIC_FN_ONLY
 static inline void leave(etiss_int32 priv_lvl);
@@ -168,6 +136,18 @@ static inline etiss_uint64 fclass_d(etiss_uint64);
 #endif
 
 #ifndef ETISS_ARCH_STATIC_FN_ONLY
+etiss_uint64 etiss_get_cycles(ETISS_CPU * const cpu, ETISS_System * const system, void * const * const plugin_pointers);
+#endif
+
+#ifndef ETISS_ARCH_STATIC_FN_ONLY
+etiss_uint64 etiss_get_time();
+#endif
+
+#ifndef ETISS_ARCH_STATIC_FN_ONLY
+etiss_uint64 etiss_get_instret(ETISS_CPU * const cpu, ETISS_System * const system, void * const * const plugin_pointers);
+#endif
+
+#ifndef ETISS_ARCH_STATIC_FN_ONLY
 static inline etiss_uint32 csr_read(ETISS_CPU * const cpu, ETISS_System * const system, void * const * const plugin_pointers, etiss_uint32 csr)
 {
 if (csr == 1U) {
@@ -175,6 +155,24 @@ return *((RV32IMACFD*)cpu)->CSR[3U] & 31U;
 }
 if (csr == 2U) {
 return (*((RV32IMACFD*)cpu)->CSR[3U] >> 5U) & 7U;
+}
+if (csr == 3072U) {
+return etiss_get_cycles(cpu, system, plugin_pointers);
+}
+if (csr == 3200U) {
+return etiss_get_cycles(cpu, system, plugin_pointers) >> 32U;
+}
+if (csr == 3073U) {
+return etiss_get_time();
+}
+if (csr == 3201U) {
+return etiss_get_time() >> 32U;
+}
+if (csr == 3074U) {
+return etiss_get_instret(cpu, system, plugin_pointers);
+}
+if (csr == 3202U) {
+return etiss_get_instret(cpu, system, plugin_pointers) >> 32U;
 }
 return *((RV32IMACFD*)cpu)->CSR[csr];
 }
