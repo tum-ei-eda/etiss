3686866b4fd1fc29293aef1ab3aba2952dbcf92a
wysiwyng <4764286+wysiwyng@users.noreply.github.com>
2022-06-22 10:08:02 +0200

    improve time query

--- src/jitlibs/CSRCounters/CSRCounters.cpp
+++ src/jitlibs/CSRCounters/CSRCounters.cpp
@@ -8,7 +8,7 @@
 using namespace etiss;
 
 extern "C"
-{   
+{
 
     uint64 etiss_get_cycles(ETISS_CPU * const cpu, ETISS_System * const system, void * const * const plugin_pointers)
     {
@@ -16,12 +16,13 @@ extern "C"
     }
 
     uint64 etiss_get_time()
-    {   
-        auto steady_now = std::chrono::steady_clock::now();
-        return (uint64_t)steady_now.time_since_epoch().count();
+    {
+        auto steady_now = std::chrono::system_clock::now().time_since_epoch();
+        auto us = std::chrono::duration_cast<std::chrono::microseconds>(steady_now);
+        return (uint64)us.count();
     }
 
-    
+
     uint64 etiss_get_instret(ETISS_CPU * const cpu, ETISS_System * const system, void * const * const plugin_pointers)
     {
         return cpu->cpuTime_ps / cpu->cpuCycleTime_ps;
