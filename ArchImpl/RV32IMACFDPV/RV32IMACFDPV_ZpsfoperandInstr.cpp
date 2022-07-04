fc1ea80e2fc0af0bf31b3a409dec2875c27cef1b
Philipp van Kempen <philipp.van-kempen@tum.de>
2022-07-04 10:39:19 +0200

    RVP: implement SMAR64 instruction

--- ArchImpl/RV32IMACFDPV/RV32IMACFDPV_ZpsfoperandInstr.cpp
+++ ArchImpl/RV32IMACFDPV/RV32IMACFDPV_ZpsfoperandInstr.cpp
@@ -14,6 +14,80 @@ using namespace etiss;
 using namespace etiss::instr;
 
 
+// SMAR64 ----------------------------------------------------------------------
+static InstructionDefinition smar64_rd_rs1_rs2 (
+	ISA32_RV32IMACFDPV,
+	"smar64",
+	(uint32_t) 0x84001077,
+	(uint32_t) 0xfe00707f,
+	[] (BitArray & ba,etiss::CodeSet & cs,InstructionContext & ic)
+	{
+
+// -----------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------
+etiss_uint32 rd = 0;
+static BitArrayRange R_rd_0(11, 7);
+rd += R_rd_0.read(ba) << 0;
+etiss_uint32 rs1 = 0;
+static BitArrayRange R_rs1_0(19, 15);
+rs1 += R_rs1_0.read(ba) << 0;
+etiss_uint32 rs2 = 0;
+static BitArrayRange R_rs2_0(24, 20);
+rs2 += R_rs2_0.read(ba) << 0;
+
+// -----------------------------------------------------------------------------
+
+		CodePart & partInit = cs.append(CodePart::INITIALREQUIRED);
+
+		partInit.code() = std::string("//SMAR64\n");
+
+// -----------------------------------------------------------------------------
+partInit.code() += "cpu->instructionPointer = " + std::to_string(ic.current_address_ + 4U) + ";\n";
+partInit.code() += "etiss_int32 rs1_val = *((RV32IMACFDPV*)cpu)->X[" + std::to_string(rs1) + "];\n";
+partInit.code() += "etiss_int32 rs2_val = *((RV32IMACFDPV*)cpu)->X[" + std::to_string(rs2) + "];\n";
+partInit.code() += "etiss_int64 temp = (((*((RV32IMACFDPV*)cpu)->X[" + std::to_string((((rd) >> (1U)) & 15) * 2U + 1U) + "]) << 32) | (*((RV32IMACFDPV*)cpu)->X[" + std::to_string((((rd) >> (1U)) & 15) * 2U) + "]));\n";
+partInit.code() += "etiss_int64 temp2 = temp + rs1_val * rs2_val;\n";
+partInit.code() += "*((RV32IMACFDPV*)cpu)->X[" + std::to_string((((rd) >> (1U)) & 15) * 2U + 1U) + "] = (((temp2) >> (32U)) & 4294967295);\n";
+partInit.code() += "*((RV32IMACFDPV*)cpu)->X[" + std::to_string((((rd) >> (1U)) & 15) * 2U) + "] = (((temp2) >> (0U)) & 4294967295);\n";
+// -----------------------------------------------------------------------------
+
+		partInit.getRegisterDependencies().add(reg_name[(((rd) >> (1U)) & 15) * 2U], 32);
+		partInit.getRegisterDependencies().add(reg_name[(((rd) >> (1U)) & 15) * 2U + 1U], 32);
+		partInit.getRegisterDependencies().add(reg_name[rs1], 32);
+		partInit.getRegisterDependencies().add(reg_name[rs2], 32);
+		partInit.getAffectedRegisters().add(reg_name[(((rd) >> (1U)) & 15) * 2U], 32);
+		partInit.getAffectedRegisters().add(reg_name[(((rd) >> (1U)) & 15) * 2U + 1U], 32);
+		partInit.getAffectedRegisters().add("instructionPointer", 32);
+
+		return true;
+	},
+	0,
+	[] (BitArray & ba, Instruction & instr)
+	{
+// -----------------------------------------------------------------------------
+etiss_uint32 rd = 0;
+static BitArrayRange R_rd_0(11, 7);
+rd += R_rd_0.read(ba) << 0;
+etiss_uint32 rs1 = 0;
+static BitArrayRange R_rs1_0(19, 15);
+rs1 += R_rs1_0.read(ba) << 0;
+etiss_uint32 rs2 = 0;
+static BitArrayRange R_rs2_0(24, 20);
+rs2 += R_rs2_0.read(ba) << 0;
+
+// -----------------------------------------------------------------------------
+
+		std::stringstream ss;
+// -----------------------------------------------------------------------------
+ss << "smar64" << " # " << ba << (" [rd=" + std::to_string(rd) + " | rs1=" + std::to_string(rs1) + " | rs2=" + std::to_string(rs2) + "]");
+// -----------------------------------------------------------------------------
+		return ss.str();
+	}
+);
+
 // MULR64 ----------------------------------------------------------------------
 static InstructionDefinition mulr64_rd_rs1_rs2 (
 	ISA32_RV32IMACFDPV,
