e2127fc70cb2d2f6b6535808661685534327d0f5
Daniel Schloms <daniel.schloms@gmail.com>
2025-01-22 10:43:12 +0000

    Fix unit-strided segment loads

--- src/jitlibs/softvector/etiss_softvector.cpp
+++ src/jitlibs/softvector/etiss_softvector.cpp
@@ -48,7 +48,8 @@ uint8_t etiss_vload_encoded_unitstride(
     (*(pSystem->dread))(pSystem->handle, pCpu, addr, buff, len);
   };
 
-  VLSU::load_eew(f_readMem, VectorRegField, _z_emul, _n_emul, pEEW/8, pVL, pVLEN/8, pVd, pMSTART, pVSTART, pVm, pEEW/8);
+  auto const eew_bytes = pEEW >> 3U;
+  VLSU::load_eew(f_readMem, VectorRegField, _z_emul, _n_emul, eew_bytes, pVL, pVLEN/8, pVd, pMSTART, pVSTART, pVm, eew_bytes);
   return (0);
 }
 
@@ -72,7 +73,8 @@ uint8_t etiss_vload_encoded_stride(
     (*(pSystem->dread))(pSystem->handle, pCpu, addr, buff, len);
   };
 
-  VLSU::load_eew(f_readMem, VectorRegField, _z_emul, _n_emul, pEEW/8, pVL, pVLEN/8, pVd, pMSTART, pVSTART, pVm, pSTRIDE);
+  auto const eew_bytes = pEEW >> 3U;
+  VLSU::load_eew(f_readMem, VectorRegField, _z_emul, _n_emul, eew_bytes, pVL, pVLEN/8, pVd, pMSTART, pVSTART, pVm, pSTRIDE);
 
   return (0);
 }
@@ -99,10 +101,12 @@ uint8_t etiss_vload_segment_unitstride(
 
   uint16_t _vstart = pVSTART;
   uint64_t _moffset = pMSTART;
-  for(int i = 0; i< pNF; ++i){
-    VLSU::load_eew(f_readMem, VectorRegField, _z_emul, _n_emul, pEEW/8, pVL, pVLEN/8, pVd + (i*_z_emul/_n_emul), _moffset, _vstart, pVm, 0);
-    _moffset+= (pVL-_vstart)*pEEW/8;
-    _vstart = 0;
+  auto const eew_bytes = pEEW >> 3U;
+
+  for(int i = 0; i < pNF; ++i){
+    VLSU::load_eew(f_readMem, VectorRegField, _z_emul, _n_emul, eew_bytes, pVL, pVLEN/8, pVd + (i*_z_emul/_n_emul), _moffset, _vstart, pVm, pNF * eew_bytes);
+    _moffset += eew_bytes;
+    // _vstart = 0;
   }
   return (0);
 }
@@ -129,10 +133,12 @@ uint8_t etiss_vload_segment_stride(
 
   uint16_t _vstart = pVSTART;
   uint64_t _moffset = pMSTART;
-  for(int i = 0; i< pNF; ++i){
-    _moffset = pMSTART + i*pEEW/8;
-    VLSU::load_eew(f_readMem, VectorRegField, _z_emul, _n_emul, pEEW/8, pVL, pVLEN/8, pVd + (i*_z_emul/_n_emul), _moffset, _vstart, pVm, pSTRIDE);
-    _vstart = 0;
+  auto const eew_bytes = pEEW >> 3U;
+
+  for(int i = 0; i < pNF; ++i){
+    _moffset = pMSTART + i * (eew_bytes);
+    VLSU::load_eew(f_readMem, VectorRegField, _z_emul, _n_emul, eew_bytes, pVL, pVLEN/8, pVd + (i*_z_emul/_n_emul), _moffset, _vstart, pVm, pSTRIDE);
+    // _vstart = 0;
   }
   return (0);
 }
@@ -147,6 +153,7 @@ uint8_t etiss_vstore_encoded_unitstride(
   VTYPE::VTYPE _vt(pVTYPE);
   uint64_t _z_emul = pEEW*_vt._z_lmul;
   uint64_t _n_emul = _vt._sew*_vt._n_lmul;
+  auto const eew_bytes = pEEW >> 3U;
 
   if ((_n_emul > _z_emul*8) || (_z_emul > _n_emul*8)) return 1;
 
@@ -155,7 +162,7 @@ uint8_t etiss_vstore_encoded_unitstride(
     (*(pSystem->dwrite))(pSystem->handle, pCpu, addr, buff, len);
   };
 
-  VLSU::store_eew(f_writeMem, VectorRegField, _z_emul, _n_emul, pEEW/8, pVL, pVLEN/8, pVd, pMSTART, pVSTART, pVm, pEEW/8);
+  VLSU::store_eew(f_writeMem, VectorRegField, _z_emul, _n_emul, eew_bytes, pVL, pVLEN/8, pVd, pMSTART, pVSTART, pVm, eew_bytes);
 
   return (0);
 }
@@ -170,6 +177,7 @@ uint8_t etiss_vstore_encoded_stride(
   VTYPE::VTYPE _vt(pVTYPE);
   uint64_t _z_emul = pEEW*_vt._z_lmul;
   uint64_t _n_emul = _vt._sew*_vt._n_lmul;
+  auto const eew_bytes = pEEW >> 3U;
 
   if ((_n_emul > _z_emul*8) || (_z_emul > _n_emul*8)) return 1;
 
@@ -178,7 +186,7 @@ uint8_t etiss_vstore_encoded_stride(
     (*(pSystem->dwrite))(pSystem->handle, pCpu, addr, buff, len);
   };
 
-  VLSU::store_eew(f_writeMem, VectorRegField, _z_emul, _n_emul, pEEW/8, pVL, pVLEN/8, pVd, pMSTART, pVSTART, pVm, pStride);
+  VLSU::store_eew(f_writeMem, VectorRegField, _z_emul, _n_emul, eew_bytes, pVL, pVLEN/8, pVd, pMSTART, pVSTART, pVm, pStride);
 
   return (0);
 }
@@ -191,10 +199,11 @@ uint8_t etiss_vstore_segment_unitstride(
   uint16_t pVTYPE, uint8_t  pVm, uint16_t pEEW, uint8_t pNF, uint8_t pVd, uint16_t pVSTART, uint16_t pVLEN, uint16_t pVL, uint64_t pMSTART)
 {
   VTYPE::VTYPE _vt(pVTYPE);
-  uint64_t _z_emul = pEEW*_vt._z_lmul;
-  uint64_t _n_emul = _vt._sew*_vt._n_lmul;
+  uint64_t _z_emul = pEEW * _vt._z_lmul;
+  uint64_t _n_emul = _vt._sew * _vt._n_lmul;
+  auto const eew_bytes = pEEW >> 3U;
 
-  if ((_n_emul > _z_emul*pNF*8) || (_z_emul*pNF > _n_emul*8)) return 1;
+  if ((_n_emul > _z_emul * pNF * 8) || (_z_emul * pNF > _n_emul * 8)) return 1;
   if ((pVd + pNF*_z_emul/_n_emul) > 32) return 1;
   if (pVSTART >= pVL) return (0);
 
@@ -206,8 +215,8 @@ uint8_t etiss_vstore_segment_unitstride(
   uint16_t _vstart = pVSTART;
   uint64_t _moffset = pMSTART;
   for(int i = 0; i< pNF; ++i){
-    VLSU::store_eew(f_writeMem, VectorRegField, _z_emul, _n_emul, pEEW/8, pVL, pVLEN/8, pVd + (i*_z_emul/_n_emul), _moffset, _vstart, pVm, pNF * (pEEW/8));
-    _moffset+= pEEW/8;
+    VLSU::store_eew(f_writeMem, VectorRegField, _z_emul, _n_emul, eew_bytes, pVL, pVLEN/8, pVd + (i * _z_emul / _n_emul), _moffset, _vstart, pVm, pNF * eew_bytes);
+    _moffset += eew_bytes;
     // TODO: Why reset _vstart?
     // _vstart = 0;
   }
@@ -222,11 +231,11 @@ uint8_t etiss_vstore_segment_stride(
   uint16_t pVTYPE, uint8_t pVm, uint16_t pEEW, uint8_t pNF, uint8_t pVd, uint16_t pVSTART, uint16_t pVLEN, uint16_t pVL, uint64_t pMSTART, int16_t pStride)
 {
   VTYPE::VTYPE _vt(pVTYPE);
-  uint64_t _z_emul = pEEW*_vt._z_lmul;
-  uint64_t _n_emul = _vt._sew*_vt._n_lmul;
+  uint64_t _z_emul = pEEW * _vt._z_lmul;
+  uint64_t _n_emul = _vt._sew * _vt._n_lmul;
 
-  if ((_n_emul > _z_emul*pNF*8) || (_z_emul*pNF > _n_emul*8)) return 1;
-  if ((pVd + pNF*_z_emul/_n_emul) > 32) return 1;
+  if ((_n_emul > _z_emul * pNF * 8) || (_z_emul * pNF > _n_emul * 8)) return 1;
+  if ((pVd + pNF * _z_emul / _n_emul) > 32) return 1;
   if (pVSTART >= pVL) return (0);
 
   uint8_t* VectorRegField = pV;
@@ -236,6 +245,7 @@ uint8_t etiss_vstore_segment_stride(
 
   uint16_t _vstart = pVSTART;
   uint64_t _moffset = pMSTART;
+  auto const eew_bytes = pEEW >> 3U;
 
   // Memory order problem: 
   // Which stores come first? According to the spec: 
@@ -243,18 +253,15 @@ uint8_t etiss_vstore_segment_stride(
   // including the case where the byte stride is such that segments overlap in memory."
   // For now, we change order depending on positive/negative stride.
   if (pStride < 0){
-    for(int i = 0; i < pNF; i++) {
-      // _moffset = pMSTART + i*pEEW/8;
-      VLSU::store_eew(f_writeMem, VectorRegField, _z_emul, _n_emul, pEEW/8, pVL, pVLEN/8, pVd + (i*_z_emul/_n_emul), _moffset, _vstart, pVm, pStride);
-      _moffset+= pEEW/8;
-      // _vstart = 0;
+    for(int i = 0; i < pNF; ++i) {
+      VLSU::store_eew(f_writeMem, VectorRegField, _z_emul, _n_emul, eew_bytes, pVL, pVLEN / 8, pVd + (i * _z_emul / _n_emul), _moffset, _vstart, pVm, pStride);
+      _moffset += eew_bytes;
     }
   }
   else{
-    for(int i = pNF - 1; i >= 0; i--) {
-      _moffset = pMSTART + i * (pEEW / 8);
-      VLSU::store_eew(f_writeMem, VectorRegField, _z_emul, _n_emul, pEEW/8, pVL, pVLEN/8, pVd + (i * (_z_emul / _n_emul)), _moffset, _vstart, pVm, pStride);
-      // _vstart = 0;
+    for(int i = pNF - 1; i >= 0; --i) {
+      _moffset = pMSTART + (i * eew_bytes);
+      VLSU::store_eew(f_writeMem, VectorRegField, _z_emul, _n_emul, eew_bytes, pVL, pVLEN / 8, pVd + (i * _z_emul / _n_emul), _moffset, _vstart, pVm, pStride);
     }
   }
   return (0);
