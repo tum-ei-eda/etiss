16f1ff6c7f690855fdac89ecea728da56455f17c
Heidi Holappa <heidi.holappa@helsinki.fi>
2025-05-06 13:08:03 +0200

    Add first working version of reading register information

--- src/IntegratedLibrary/ISAExtensionValidator.cpp
+++ src/IntegratedLibrary/ISAExtensionValidator.cpp
@@ -4,6 +4,8 @@
 
 
 #include "etiss/IntegratedLibrary/ISAExtensionValidator.h"
+
+#include "../../ArchImpl/RV32IMACFD/RV32IMACFD.h"
 #include "etiss/CPUArch.h"
 
 using namespace etiss::plugin;
@@ -22,20 +24,40 @@ void ISAExtensionValidator::finalizeInstrSet(etiss::instr::ModedInstructionSet &
             set.foreach ([](etiss::instr::Instruction &instr) {
                 instr.addCallback(
                     [&instr](etiss::instr::BitArray &ba, etiss::CodeSet &cs, etiss::instr::InstructionContext &ic) {
-                        std::stringstream ss;
+                        // std::stringstream ss;
 
                         // Taken from RV32IMACFD_RV32Instr.cpp
-                        etiss_uint8 rd = 0;
+                        etiss_uint8 rd {0};
                         static BitArrayRange R_rd_0(11, 7);
-                        rd += R_rd_0.read(ba) << 0;
 
-                        ss << "ISAExtensionValidation_print(\"*((RV32IMACFD*)cpu)->X[";
-                        ss << "Here we want processor state data. Like: ";
-                        ss << std::to_string(rd % 32ULL) + "ULL";
-                        ss << "]\\n\");\n";
+                        /*ss << "ISAExtensionValidation_print(\"*((RV32IMACFD*)cpu)->X[";
+                        // Let's try to output X[x_0, ... x_31]
+                        for (int i = 0; i < 32; ++i)
+                        {
+                            rd += R_rd_0.read(ba) << 0;
+                            ss << std::to_string(rd % 32ULL) + "ULL";
+                            if (i != 31) ss << ", ";
+                        }
+                        ss << "]\\n\");\n";*/
+
+                        for (int i = 0; i < 32; i++)
+                        {
+                            std::stringstream ss;
+                            rd += R_rd_0.read(ba) << 0;
+                            ss << "etiss_uint32 register_value_" << std::to_string(i) << ";\n";
+                            ss << "register_value_" << std::to_string(i) << " = (etiss_uint32)(*((RV32IMACFD*)cpu)->X[" << std::to_string(rd % 32ULL) + "ULL]);\n";
+                            ss << "ISAExtensionValidation_print_int(" << i << ", register_value_" << std::to_string(i) << ");\n;";
+                            cs.append(CodePart::PREINITIALDEBUGRETURNING).code() = ss.str();
+                        }
 
 
-                        cs.append(CodePart::PREINITIALDEBUGRETURNING).code() = ss.str();
+
+                        /*ss << "etiss_uint32 mem_val_0;";
+                        ss << "mem_val_0 = (etiss_uint32)(*((RV32IMACFD*)cpu)->X[1ULL]);";*/
+
+
+
+                        // cs.append(CodePart::PREINITIALDEBUGRETURNING).code() = ss.str();
 
                         return true;
                     },
@@ -49,6 +71,7 @@ void ISAExtensionValidator::initCodeBlock(etiss::CodeBlock &block ) const
 {
     std::cout << "ISAExtensionValidator::initCodeBlock" << std::endl;
     block.fileglobalCode().insert("extern void ISAExtensionValidation_print(const char *);"); // add print function
+    block.fileglobalCode().insert("extern void ISAExtensionValidation_print_int(int, etiss_uint32);"); // add print function
 };
 
 void ISAExtensionValidator::finalizeCodeBlock(etiss::CodeBlock & ) const
@@ -72,5 +95,13 @@ extern "C"
     {
         std::cout << c;
         std::cout.flush();
+    }
+}
+
+extern "C"
+{
+    void ISAExtensionValidation_print_int(int x, etiss_uint32 r)
+    {
+        printf("X[%d]: %d", x, r);
     }
 }
