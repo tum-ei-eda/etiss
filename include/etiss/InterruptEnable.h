5dc48f19f85d346ebd65d8795bf12e82d21b6fc0
JoGei <62236820+JoGei@users.noreply.github.com>
2025-09-24 16:44:45 +0200

    Refactor license headers  (#181)
    * Replace long BSD-3 clause boilerplate license header in source code with shortened version referencing <root>/LICENSE
    
    * Add TUM as a contributor to the copyright holders with the year as a range

--- include/etiss/InterruptEnable.h
+++ include/etiss/InterruptEnable.h
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// This file is part of ETISS. It is licensed under the BSD 3-Clause License; you may not use this file except in
+// compliance with the License. You should have received a copy of the license along with this project. If not, see the
+// LICENSE file.
+
 #ifndef ETISS_INCLUDE_INTERRUPTENABLE_H_
 #define ETISS_INCLUDE_INTERRUPTENABLE_H_
 
--- include/etiss/make_unique.h
+++ include/etiss/make_unique.h
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// This file is part of ETISS. It is licensed under the BSD 3-Clause License; you may not use this file except in
+// compliance with the License. You should have received a copy of the license along with this project. If not, see the
+// LICENSE file.
+
 #ifndef MAKE_UNIQUE_H
 #define MAKE_UNIQUE_H
 
