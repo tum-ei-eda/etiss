3a00873b5e9a28a0ac8947ad0852741d09faeea1
JoGei <62236820+JoGei@users.noreply.github.com>
2025-11-12 13:37:12 +0100

    transfer ownershop of DiagnosticConsumer to DiagnosticEngine

--- JITImpl/LLVM/src/llvm_compat/v20/CompatLLVMJIT.cpp
+++ JITImpl/LLVM/src/llvm_compat/v20/CompatLLVMJIT.cpp
@@ -50,8 +50,7 @@ void createDiagnostics(clang::CompilerInstance &CI)
     auto diagPrinter = std::make_unique<clang::TextDiagnosticPrinter>(llvm::errs(), &*diagOpts);
 
     llvm::IntrusiveRefCntPtr<clang::DiagnosticsEngine> diags(
-        new clang::DiagnosticsEngine(diagID, diagOpts, diagPrinter.get(), false));
-
+            new clang::DiagnosticsEngine(diagID, diagOpts, diagPrinter.release(), false));
     CI.setDiagnostics(&*diags);
 }
 } // namespace compat
